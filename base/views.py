d3e831cbdb4aa677bd0d7210798c986da4a204ad
cgrade <gradeisageek@gmail.com>
2023-03-06 20:07:00 +0100

    studybudy chat web app

--- base/views.py
+++ base/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
+from .models import Room
 # from django.http import HttpResponse
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets Learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend Developer'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets Learn python!'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend Developer'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == pk:
-            room = i
+    room = Room.objects.get(id=pk)
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == pk:
+    #         room = i
     context = {'room': room}
     return render(request, 'base/room.html', context)
