c8346a095044cd986eb22df90d57527eb08e8d5d
cgrade <gradeisageek@gmail.com>
2023-03-07 01:24:39 +0100

    View functions for 'Create' and 'Edit' rom added to manage rooms

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -19,9 +20,35 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    # room = None
-    # for i in rooms:
-    #     if i['id'] == pk:
-    #         room = i
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
