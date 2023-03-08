07750f2eef9e41c60d9a2be5e88ef4d2e2646caa
cgrade <gradeisageek@gmail.com>
2023-03-08 01:22:33 +0100

    loginPage view function added to handle the authentication, flash message and redirection during login sessions

--- base/views.py
+++ base/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
+
+
+
+
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -11,11 +19,36 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend Developer'},
 # ]
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None: 
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password not found')
+            return redirect('login')
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 
 def home(request):
-
+ 
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
 
     topics = Topic.objects.all()
     room_count = rooms.count()
