4d74c97c3a1c9fa80a76baa4822dd122be7de5a2
cgrade <gradeisageek@gmail.com>
2023-03-08 01:25:01 +0100

    url path to loginPage added

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name='login'),
     path('', views.home, name='home'),
-    path('room/<int:pk>', views.room, name='room')
+    path('room/<int:pk>', views.room, name='room'),
+    path('update-room/<int:pk>', views.updateRoom, name='update-room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('delete-room/<int:pk>', views.deleteRoom, name='delete-room')
 ]
