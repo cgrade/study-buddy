97d7b792ae15dccd14515e332200ffeca23886c4
cgrade <gradeisageek@gmail.com>
2023-03-25 23:51:47 +0100

    api added

---
+++ base/api/views.py
@@ -0,0 +1,11 @@
+from django.http import JsonResponse
+
+
+def getRoute(request):
+    routes = [
+        'GET /api'
+        'GET /api/rooms',
+        'GET /api/rooms/:id'
+
+    ]
+    return JsonResponse(routes, safe=False)
